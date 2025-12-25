06591ff93f811f6bc4d4d7b727c1e5f85242cc33
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-12-25 20:38:20 +0300

    Fetched the data from the fake api using the useEffect  and displayed a portion of the data for testing utilizing the error and loading state

--- ShoppingCart/src/pages/shop.jsx
+++ ShoppingCart/src/pages/shop.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+
 export default function Shop(){
+
+    const [shopData, setShopData] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products/')
+        .then((data) => {
+            if (data.status >= 400){
+                throw new Error("Server Error")
+            }
+            return data.json()
+        })
+        .then(data => setShopData(data))
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false))
+
+    }, [])
+
+    console.log(shopData)
+    if (loading) return <p>Loadong ... </p>
+    if(error) return <p>Network Error encountered</p>
+
     return (
         <div>
         <h2>Welcome to the Shop</h2>
+        <h3>{shopData[4].title}</h3>
         </div>
     )
 }
